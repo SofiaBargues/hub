4148c294cb74d300f5791fbd8cfc357088c7dbe9
Sofia Bargues <barguessofia@gmail.com>
2023-11-22 11:14:51 +0000

    Fetching Games

---
+++ src/components/GameGrid.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import apiClient from "../service/api-client";
+import { Text, list } from "@chakra-ui/react";
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface FechinGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+export const GameGrid = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    apiClient
+      .get<FechinGamesResponse>("/games")
+      .then((res) => setGames(res.data.results))
+      .catch((err) => setError(err.message));
+  });
+
+  return (
+    <>
+      <Text>{error}</Text>
+      <ul>
+        {games.map((game) => (
+          <li key={game.id}>{game.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
---
+++ src/service/api-client.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+console.log({ key: import.meta.env });
+
+export default axios.create({
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: import.meta.env.VITE_RAWG_API_KEY,
+  },
+});
