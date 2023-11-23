4996145aae3ca807d46b365e3d48f225bb2e0128
Sofia Bargues <barguessofia@gmail.com>
2023-11-23 10:52:11 +0000

    Show loading skeletons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,9 +1,13 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
+import { SiDigikeyelectronics } from "react-icons/si";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 export const GameGrid = () => {
-  const { games, error } = useGames();
+  const { games, error, isLoading } = useGames();
+  const skeleton = [1, 2, 3, 4, 5, 6];
+
   return (
     <>
       <Text>{error}</Text>
@@ -12,6 +16,8 @@ export const GameGrid = () => {
         padding="10px"
         spacing={10}
       >
+        {isLoading &&
+          skeleton.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
         {games.map((game) => (
           <GameCard key={game.id} game={game} />
         ))}
--- src/hooks/useGames.tsx
+++ src/hooks/useGames.tsx
@@ -25,10 +25,12 @@ interface FechinGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
     // Use fake API during development to save credits
     setGames(RAWG_RESPONSE);
+    setLoading(false);
     // Early return for fake API
     return;
 
@@ -36,14 +38,18 @@ const useGames = () => {
 
     apiClient
       .get<FechinGamesResponse>("/games", { signal: controller.signal })
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false);
       });
 
     return () => controller.abort();
   }, []);
-  return { games, error };
+  return { games, error, isLoading };
 };
 export default useGames;
